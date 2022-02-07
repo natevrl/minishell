a27984fab4862769951a20b2eeb2e0ed404e096c
Nabentay <nabentay@student.42.fr>
2022-02-07 19:44:13 +0000

    Builtin

--- builtin/load_builtin.c
+++ builtin/load_builtin.c
@@ -6,17 +6,32 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:25:21 by ubuntu            #+#    #+#             */
-/*   Updated: 2022/02/07 18:27:08 by ubuntu           ###   ########.fr       */
+/*   Updated: 2022/02/07 18:33:15 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+void	list_push(t_list **lst_addr, void *data)
+{
+	t_list	**ptr;
+
+	ptr = lst_addr;
+	while (*ptr)
+		ptr = &(*ptr)->next;
+	*ptr = (t_list *)malloc(sizeof(t_list));
+	(*ptr)-> = data;
+	(*ptr)->next = NULL;
+}
+
 void	ft_load_builtin(t_builtin **list, t_fun *fun,char *name)
 {
+	t_builtin *t;
 
-	(*list)->name = name;
-	(*list)->built = fun;
+	t = (t_builtin *)malloc(sizeof(t_builtin));
+	t->name = name;
+	t->built = fun;
+	list_push(list, t);
 }
 
 void	ft_bultin(t_list *cmd)
