e7ca889963260a4fc65ebb3115963100a3fd8397
Nabentay <nabentay@student.42.fr>
2022-02-12 10:19:22 +0000

    launch builtin improved

--- builtin/load_builtin.c
+++ builtin/load_builtin.c
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:25:21 by ubuntu            #+#    #+#             */
-/*   Updated: 2022/02/11 23:06:26 by ubuntu           ###   ########.fr       */
+/*   Updated: 2022/02/12 10:18:37 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,16 @@ void	ft_load_builtin(t_list **list, t_fun *fun, t_ls *ls, char *name)
 	list_push(list, t);
 }
 
+void	ft_launch_builtin(t_list *list, t_list *cmd, char *name)
+{
+	while (ft_strncmp(((t_builtin *)list->content)->name, name, ft_strlen(name)) != 0)
+		list = list->next;
+	if (((t_builtin *)list->content)->built != NULL)
+		((t_builtin *)list->content)->built(cmd->arg);
+	else
+		((t_builtin *)list->content)->built_l(cmd);
+}
+
 void	ft_bultin(t_list **tmp)
 {
 	t_list	*list;
@@ -49,33 +59,13 @@ void	ft_bultin(t_list **tmp)
 	while (cmd != NULL)
 	{
 		if (ft_strncmp("echo", cmd->arg[0], 4) == 0)
-		{
-			if (ft_strncmp(((t_builtin *)list->content)->name, "echo", 4) == 0)
-				((t_builtin *)list->content)->built(cmd->arg);
-			else
-				list = list->next;
-		}
+			ft_launch_builtin(list, cmd, "echo");
 		else if (ft_strncmp("pwd", cmd->arg[0], 4) == 0)
-		{
-			if (ft_strncmp(((t_builtin *)list->content)->name, "pwd", 4) == 0)
-				((t_builtin *)list->content)->built(cmd->arg);
-			else
-				list = list->next;
-		}
+			ft_launch_builtin(list, cmd, "pwd");
 		else if (ft_strncmp("env", cmd->arg[0], 3) == 0)
-		{
-			if (ft_strncmp(((t_builtin *)list->content)->name, "env", 3) == 0)
-				((t_builtin *)list->content)->built_l(cmd);
-			else
-				list = list->next;
-		}
+			ft_launch_builtin(list, cmd, "env");
 		else if (ft_strncmp("cd", cmd->arg[0], 2) == 0)
-		{
-			if (ft_strncmp(((t_builtin *)list->content)->name, "cd", 2) == 0)
-				((t_builtin *)list->content)->built_l(cmd);
-			else
-				list = list->next;
-		}
+			ft_launch_builtin(list, cmd, "cd");
 		cmd = cmd->next;
 	}
 }
