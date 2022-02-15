796c227014790d889dc3dcb5f4c0b7ab4b7ea928
EC2 Default User <ec2-user@ip-10-8-6-160.eu-west-3.compute.internal>
2022-02-15 12:40:26 +0000

    add gnl

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nabentay <nabentay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 13:55:16 by nabentay          #+#    #+#             */
+/*   Updated: 2021/12/09 13:18:35 by nabentay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+char	*ft_joinline(ssize_t i, char **file);
+char	*ft_line(int fd, char **file, char *buff);
+char	*get_next_line(int fd);
+
+#endif
