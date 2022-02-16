ad9d9e16ecd468a4b2360ec626b0cbdb4732d843
Nabentay <nabentay@student.42.fr>
2022-02-16 14:49:00 +0000

    minishell

--- builtin/exit.c
+++ builtin/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nabentay <nabentay@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 10:58:04 by ubuntu            #+#    #+#             */
-/*   Updated: 2022/02/16 05:23:39 by nabentay         ###   ########.fr       */
+/*   Updated: 2022/02/16 14:48:30 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ void	built_exit(t_list **cmd)
 	while (list->arg[i])
 		i++;
 	if (i == 1)
-		exit(2);
+		ft_exit(2);
 	i = 0;
 	while (list->arg[1][i])
 	{
 		if (ft_isalpha(list->arg[1][i]))
 		{
 			write(1, "numeric argument required\n", 26);
-			exit(2);
+			ft_exit(2);
 		}
 		i++;
 	}
@@ -39,8 +39,7 @@ void	built_exit(t_list **cmd)
 	if (i > 2)
 	{
 		write(2, "exit: too many arguments\n", 25);
-		exit(5);
+		ft_exit(5);
 	}
-	g_err = ft_atoi(list->arg[1]);
-	exit(g_err);
+	ft_exit(ft_atoi(list->arg[1]));
 }
