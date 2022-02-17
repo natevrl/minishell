cb9f61947a2ffd4f7e1dd92eb4787722e3bcea2f
Nabentay <nabentay@student.42.fr>
2022-02-17 14:56:15 +0000

    Signal multiple minishell handle

--- signal/signaux.c
+++ signal/signaux.c
@@ -6,20 +6,37 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 13:47:54 by ubuntu            #+#    #+#             */
-/*   Updated: 2022/02/16 14:04:38 by ubuntu           ###   ########.fr       */
+/*   Updated: 2022/02/17 14:55:43 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+void	get_pid(int sig, siginfo_t *info, void *context)
+{
+	(void)context;
+	if (info->si_pid > 0 && sig == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+		g_err = 130;
+	}
+	else
+		wait(NULL);
+}
+
 int	launch_bash(t_list *cmd)
 {
+	struct sigaction sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = get_pid;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
 	while (1)
-	{
-		signal(SIGINT, sig_handler);
-		signal(SIGQUIT, SIG_IGN);
 		prompt(cmd);
-	}
 	return (g_err);
 }
 
